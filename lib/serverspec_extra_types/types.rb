2c2be6e08a5b58ed199bce62689ad7e7d61aa00e
Andrew Wardrobe <andrew.g.wardrobe@accenture.com>
2018-10-02 14:45:56 +0000

    fix: require docker node type

--- lib/serverspec_extra_types/types.rb
+++ lib/serverspec_extra_types/types.rb
@@ -4,7 +4,7 @@ require 'serverspec/helper/type'
 module Serverspec
   module Helper
     module Type
-      types = %w[docker_service rabbitmq_vhost_policy rabbitmq_node_list rabbitmq_vhost_list
+      types = %w[docker_service docker_node rabbitmq_vhost_policy rabbitmq_node_list rabbitmq_vhost_list
                  rabbitmq_user_permission consul_service consul_service_list consul_node consul_node_list]
 
       types.each do |type|

75cb495f13c9276e05b2eb854adb0f448fa0c834
Andrew Wardrobe <andrew.g.wardrobe@googlemail.com>
2019-06-03 16:44:08 +0100

    Add deprecation warning if using "require 'serverspec_extra_types"

--- .simplecov
+++ .simplecov
@@ -1,5 +1,6 @@
 SimpleCov.start  do
   add_filter "/spec/"
+  add_filter "/lib/serverspec_extra_types/version.rb"
 end
 
 
---
+++ lib/serverspec_extra_types.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+puts "Deprecation WARNING: serverspec_extra_types has been deprecated as the entry point to the gem. please use:"
+puts "require 'serverspec-extra-types'"
+
+require 'serverspec-extra-types'
